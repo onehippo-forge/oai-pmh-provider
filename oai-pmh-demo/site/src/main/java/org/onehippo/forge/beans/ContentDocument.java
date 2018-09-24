2267a331843de2652b5c61af5a9e5138ac3206af
Saimir Muco <saimir.muco@bloomreach.com>
2018-09-24 16:49:59 +0200

    HIPFORGE-196 add oai-pmh rest spring configuration

--- oai-pmh-demo/site/src/main/java/org/onehippo/forge/beans/ContentDocument.java
+++ oai-pmh-demo/site/src/main/java/org/onehippo/forge/beans/ContentDocument.java
@@ -1,43 +1,42 @@
 package org.onehippo.forge.beans;
-/*
- * Copyright 2014-2015 Hippo B.V. (http://www.onehippo.com)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-import java.util.Calendar;
 
+import java.util.Calendar;
 import org.hippoecm.hst.content.beans.Node;
 import org.hippoecm.hst.content.beans.standard.HippoHtml;
 import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import org.onehippo.cms7.essentials.components.rest.adapters.HippoHtmlAdapter;
+import org.onehippo.forge.oaipmh.provider.api.OAIBean;
 
+@XmlRootElement(name = "contentdocument")
+@XmlAccessorType(XmlAccessType.NONE)
 @HippoEssentialsGenerated(internalName = "oaipmhdemo:contentdocument")
 @Node(jcrType = "oaipmhdemo:contentdocument")
-public class ContentDocument extends BaseDocument {
+public class ContentDocument extends BaseDocument implements OAIBean {
+    @XmlElement
     @HippoEssentialsGenerated(internalName = "oaipmhdemo:introduction")
     public String getIntroduction() {
         return getProperty("oaipmhdemo:introduction");
     }
 
+    @XmlElement
     @HippoEssentialsGenerated(internalName = "oaipmhdemo:title")
     public String getTitle() {
         return getProperty("oaipmhdemo:title");
     }
 
+    @XmlJavaTypeAdapter(HippoHtmlAdapter.class)
+    @XmlElement
     @HippoEssentialsGenerated(internalName = "oaipmhdemo:content")
     public HippoHtml getContent() {
         return getHippoHtml("oaipmhdemo:content");
     }
 
+    @XmlElement
     @HippoEssentialsGenerated(internalName = "oaipmhdemo:publicationdate")
     public Calendar getPublicationDate() {
         return getProperty("oaipmhdemo:publicationdate");
--- oai-pmh-demo/site/src/main/java/org/onehippo/forge/beans/NewsDocument.java
+++ oai-pmh-demo/site/src/main/java/org/onehippo/forge/beans/NewsDocument.java
@@ -1,36 +1,29 @@
 package org.onehippo.forge.beans;
-/*
- * Copyright 2014-2018 Hippo B.V. (http://www.onehippo.com)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-import java.util.Calendar;
 
+import java.util.Calendar;
 import org.hippoecm.hst.content.beans.Node;
 import org.hippoecm.hst.content.beans.standard.HippoDocument;
 import org.hippoecm.hst.content.beans.standard.HippoGalleryImageSet;
 import org.hippoecm.hst.content.beans.standard.HippoHtml;
 import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import org.onehippo.cms7.essentials.components.rest.adapters.HippoHtmlAdapter;
+import org.onehippo.cms7.essentials.components.rest.adapters.HippoGalleryImageAdapter;
+import org.onehippo.forge.oaipmh.provider.api.OAIBean;
 
+@XmlRootElement(name = "newsdocument")
+@XmlAccessorType(XmlAccessType.NONE)
 @HippoEssentialsGenerated(internalName = "oaipmhdemo:newsdocument")
-@Node(jcrType="oaipmhdemo:newsdocument")
-public class NewsDocument extends HippoDocument {
-
-    /**
+@Node(jcrType = "oaipmhdemo:newsdocument")
+public class NewsDocument extends HippoDocument implements OAIBean {
+    /** 
      * The document type of the news document.
      */
     public static final String DOCUMENT_TYPE = "oaipmhdemo:newsdocument";
-
     private static final String TITLE = "oaipmhdemo:title";
     private static final String DATE = "oaipmhdemo:date";
     private static final String INTRODUCTION = "oaipmhdemo:introduction";
@@ -40,85 +33,90 @@ public class NewsDocument extends HippoDocument {
     private static final String AUTHOR = "oaipmhdemo:author";
     private static final String SOURCE = "oaipmhdemo:source";
 
-    /**
+    /** 
      * Get the title of the document.
-     *
      * @return the title
      */
+    @XmlElement
     @HippoEssentialsGenerated(internalName = "oaipmhdemo:title")
     public String getTitle() {
         return getProperty(TITLE);
     }
 
-    /**
+    /** 
      * Get the date of the document.
-     *
      * @return the date
      */
+    @XmlElement
     @HippoEssentialsGenerated(internalName = "oaipmhdemo:date")
     public Calendar getDate() {
         return getProperty(DATE);
     }
 
-    /**
+    /** 
      * Get the introduction of the document.
-     *
      * @return the introduction
      */
+    @XmlElement
     @HippoEssentialsGenerated(internalName = "oaipmhdemo:introduction")
     public String getIntroduction() {
         return getProperty(INTRODUCTION);
     }
 
-    /**
+    /** 
      * Get the image of the document.
-     *
      * @return the image
      */
+    @XmlJavaTypeAdapter(HippoGalleryImageAdapter.class)
+    @XmlElement
     @HippoEssentialsGenerated(internalName = "oaipmhdemo:image")
     public HippoGalleryImageSet getImage() {
         return getLinkedBean(IMAGE, HippoGalleryImageSet.class);
     }
 
-    /**
+    /** 
      * Get the main content of the document.
-     *
      * @return the content
      */
+    @XmlJavaTypeAdapter(HippoHtmlAdapter.class)
+    @XmlElement
     @HippoEssentialsGenerated(internalName = "oaipmhdemo:content")
     public HippoHtml getContent() {
         return getHippoHtml(CONTENT);
     }
 
-    /**
+    /** 
      * Get the location of the document.
-     *
      * @return the location
      */
+    @XmlElement
     @HippoEssentialsGenerated(internalName = "oaipmhdemo:location")
     public String getLocation() {
         return getProperty(LOCATION);
     }
 
-    /**
+    /** 
      * Get the author of the document.
-     *
      * @return the author
      */
+    @XmlElement
     @HippoEssentialsGenerated(internalName = "oaipmhdemo:author")
     public String getAuthor() {
         return getProperty(AUTHOR);
     }
 
-    /**
+    /** 
      * Get the source of the document.
-     *
      * @return the source
      */
+    @XmlElement
     @HippoEssentialsGenerated(internalName = "oaipmhdemo:source")
     public String getSource() {
         return getProperty(SOURCE);
     }
 
+    @Override
+    public Calendar getPublicationDate() {
+        return getProperty("hippostdpubwf:publicationDate");
+    }
 }
-
