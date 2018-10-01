50e0dc006c32279d6cb58886b4af9b571dd2ba3b
Saimir Muco <saimir.muco@bloomreach.com>
2018-10-01 12:04:11 +0200

    HIPFORGE-196 remove old demo project, remove explicit jaxrs dependency

--- demo/site/src/main/java/org/onehippo/forge/oaipmh/beans/BaseDocument.java
+++
@@ -1,9 +0,0 @@
-package org.onehippo.forge.oaipmh.beans;
-
-import org.hippoecm.hst.content.beans.Node;
-import org.hippoecm.hst.content.beans.standard.HippoDocument;
-
-@Node(jcrType="oaipmh.demo:basedocument")
-public class BaseDocument extends HippoDocument {
-
-}
